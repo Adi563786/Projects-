ba6949c0d67289f35e62a6acd3ff8ab7ca68d801
Aditya <adityamaurya782003@gmail.com>
2025-12-23 11:39:41 +0530

    update code in watch.py

--- bharatnatyam/watch.py
+++ bharatnatyam/watch.py
@@ -1,23 +1,30 @@
 import cv2
 from ultralytics import YOLO
 from mp_hand import handmarks
+from landmark_converter_and_classification import is_hand_combined
+from landmark_converter_and_classification import landmark_convert_to_list
 
-vid = r"video.mp4"
+vid = r"C:\Projects-aditya\ladki2.mp4"
 person_model_path = r"person_best.pt"
 person = YOLO(person_model_path)
 cap = cv2.VideoCapture(vid)
 
 prev_landmarks = {"Left": None, "Right": None}
 missing_count = {"Left": 0, "Right": 0}
+previous_mediapipe_landmarks={"Left":None,"Right":None}
+
 MAX_MISSING = 3  # persist for up to 3 frames if lost
+
 alpha = 0.7      # smoothing factor
+
 frame_width = 640
 frame_height = 480
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-    frame = cv2.resize(frame, (1800, frame.shape[0]))
+    #frame = cv2.resize(frame, (640, 640))
     humans = person.predict(source=frame, imgsz=640, conf=0.80)
 
     for h in humans:
@@ -63,14 +70,19 @@ while cap.isOpened():
 
                 # Smoothing
                 prev = prev_landmarks[hand_label]
+                prev_mediapipe_landmarks=previous_mediapipe_landmarks[hand_label]
                 if prev is None or len(prev) != len(landmark_points):
                     smoothed = landmark_points
+                    pml=landmark_convert_to_list(hand_landmarks)
                 else:
                     smoothed = [(int(alpha * cx + (1 - alpha) * px),
                                  int(alpha * cy + (1 - alpha) * py))
                                 for (cx, cy), (px, py) in zip(landmark_points, prev)]
+                    pml=[((alpha*cx+(1-alpha)*px),(alpha*cy+(1-alpha)*py),(alpha*cz+(1-alpha)*pz))
+                         for (cx,cy,cz),(px,py,pz) in zip(prev_mediapipe_landmarks,landmark_convert_to_list(hand_landmarks))]
 
                 prev_landmarks[hand_label] = smoothed
+                previous_mediapipe_landmarks[hand_label]=pml
                 missing_count[hand_label] = 0  # reset missing counter
                 landmark_points = smoothed
 
@@ -99,9 +111,17 @@ while cap.isOpened():
                                 cv2.circle(frame, (x, y), radius=4, color=(0, 0, 255), thickness=-1)
                         else:
                             prev_landmarks[label] = None
+                            previous_mediapipe_landmarks[label]=None
                             missing_count[label] = 0
             # handle detection of single hand or combined  hand and classification
-
+            print("2 hand ",is_hand_combined(previous_mediapipe_landmarks["Left"],previous_mediapipe_landmarks["Right"]))
+            if is_hand_combined(previous_mediapipe_landmarks["Left"],previous_mediapipe_landmarks["Right"]):
+                if prev_landmarks["Left"] and prev_landmarks["Right"]:
+                    xcord=[x[0] for x in prev_landmarks["Left"]]+[x[0] for x in prev_landmarks["Right"]]
+                    ycord=[y[1] for y in prev_landmarks["Left"]]+[y[1] for y in prev_landmarks["Right"]]
+                    lx,hx=max(0,min(xcord)-20),max(xcord)+20
+                    ly,hy=max(0,min(ycord)-20),max(ycord)+20
+                    cv2.rectangle(frame, (lx,ly), (hx,hy), (0, 0, 0), -1)
 
         cv2.imshow("Video Frame", frame)
         
